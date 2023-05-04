a7f5511ac081ccaeb18cc38946f4b2df75ab9771
mushroomvoodoo <normeeboy@gmail.com>
2023-05-04 14:23:04 +0300

    Добавьте файлы проекта.

---
+++ Project Massives/Form1.cs
@@ -0,0 +1,21 @@
+namespace Project_Massives
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*dataGridView + кнопки (ввести из файла, вывести в файл) + массив (30x30) */
+            //индивидуальное задание: найти разность двух сформированных случайным образом массивов
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
